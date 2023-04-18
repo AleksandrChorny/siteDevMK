9db6c72363fa9b3cb4ad73457dbb507956f783c8
AleksandrChorny <chorny100284@gmail.com>
2023-04-18 21:00:48 +0300

    add fresh progect

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+//Подключение файлов js из папки 'modules'
+
+flsFunctions.isWebp();
+
+// core version + navigation, pagination modules:
+//!  import Swiper, { Navigation, Pagination } from 'swiper';
+//! const swiper = new Swiper(...);
