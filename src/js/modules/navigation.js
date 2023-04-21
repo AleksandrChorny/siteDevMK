44f86e7376a383381bc9975406b36b9e493ea889
AleksandrChorny <chorny100284@gmail.com>
2023-04-21 16:03:36 +0300

    3

--- src/js/modules/navigation.js
+++ src/js/modules/navigation.js
@@ -2,9 +2,9 @@ export function viewNavigation() {
    const burger = document.querySelector('.burger-menu');
    const navigation = document.querySelector('.main-menu');
 
-   document.addEventListener('click', viewNavigation1);
+   document.addEventListener('click', viewNavigation);
 
-   function viewNavigation1(event) {
+   function viewNavigation(event) {
       if (event.target.closest('.burger-menu')) {
          navigation.classList.toggle('_active');
          burger.classList.toggle('_action');
